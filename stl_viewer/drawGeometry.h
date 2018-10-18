9da705f643aee8f7f1fe15bc55607558270233b3
speech <twtravel@126.com>
2018-10-18 16:50:57 +0800

    add files

---
+++ stl_viewer/drawGeometry.h
@@ -0,0 +1,11 @@
+#ifndef DRAW_GEOMETRY
+#define DRAW_GEOMETRY
+
+// OpenGL specific headers
+
+#include <GLFW/glfw3.h>
+#include <GL/glu.h>       // for using gluLookAt( ... )
+
+#include <iostream> // atoi 
+#include <cmath>    // for using sin/cos
+#endif
