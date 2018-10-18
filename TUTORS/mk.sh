685653cc95a47a45486a28b1151cca7c63efa853
speech <twtravel@126.com>
2018-10-18 12:29:36 +0800

    add files

---
+++ TUTORS/mk.sh
@@ -0,0 +1,7 @@
+g++ fog.c glm.c -o fog  -lGLU -lGL -lglut
+g++ lightmaterial.c glm.c -o lightmaterial  -lGLU -lGL -lglut
+g++ lightposition.c glm.c -o lightposition  -lGLU -lGL -lglut
+g++ projection.c glm.c -o projection  -lGLU -lGL -lglut
+g++ shapes.c glm.c -o shapes  -lGLU -lGL -lglut
+g++ texture.c glm.c -o texture  -lGLU -lGL -lglut
+g++ transformation.c glm.c -o transformation  -lGLU -lGL -lglut
