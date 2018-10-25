138a7cfe03b5065da50114685cf9d880ea3bc21b
speech <twtravel@126.com>
2018-10-25 14:17:20 +0800

    add files

---
+++ Model/run.sh
@@ -0,0 +1,46 @@
+mv "allstll/actuating minor arm.STL"  allstll/actuating_minor_arm.STL
+mv "allstll/Axis14mm-1.STL"  allstll/Axis14mm-1.STL
+mv "allstll/Axis18mm-1.STL"  allstll/Axis18mm-1.STL
+mv "allstll/Axis-2.STL"  allstll/Axis-2.STL
+mv "allstll/Axis30mm-1.STL"  allstll/Axis30mm-1.STL
+mv "allstll/Axis-3.STL"  allstll/Axis-3.STL
+mv "allstll/Axis42mm-1.STL"  allstll/Axis42mm-1.STL
+mv "allstll/axis6mm.STL"  allstll/axis6mm.STL
+mv "allstll/Axis machining8mm.STL"  allstll/Axis_machining8mm.STL
+mv "allstll/backing ring of the bottom-1.STL"  allstll/backing_ring_of_the_bottom-1.STL
+mv "allstll/backing ring of the bottom.STL"  allstll/backing_ring_of_the_bottom.STL
+mv "allstll/baseborad for PCB-1.STL"  allstll/baseborad_for_PCB-1.STL
+mv "allstll/bearing bushes-1.STL"  allstll/bearing_bushes-1.STL
+mv "allstll/bearing bushes-2.STL"  allstll/bearing_bushes-2.STL
+mv "allstll/decorating borad-1.STL"  allstll/decorating_borad-1.STL
+mv "allstll/decorating borad-2.STL"  allstll/decorating_borad-2.STL
+mv "allstll/end effector-1.STL"  allstll/end_effector-1.STL
+mv "allstll/finger(active)-1.STL"  allstll/finger_active-1.STL
+mv "allstll/finger decorative piece-1.STL"  allstll/finger_decorative_piece-1.STL
+mv "allstll/finger decorative piece-2.STL"  allstll/finger_decorative_piece-2.STL
+mv "allstll/finger decorative piece-3.STL"  allstll/finger_decorative_piece-3.STL
+mv "allstll/finger decorative piece-4.STL"  allstll/finger_decorative_piece-4.STL
+mv "allstll/finger(passive)-1.STL"  allstll/finger_passive-1.STL
+mv "allstll/Gear bracket.STL"  allstll/Gear_bracket.STL
+mv "allstll/immobilization of servo-1.STL"  allstll/immobilization_of_servo-1.STL
+mv "allstll/major arm3.1-1.STL"  allstll/major_arm3.1-1.STL
+mv "allstll/minor arm3.0-1.STL"  allstll/minor_arm3.0-1.STL
+mv "allstll/paralleled brace-1.STL"  allstll/paralleled_brace-1.STL
+mv "allstll/Positioning plate-1.STL"  allstll/Positioning_plate-1.STL
+mv "allstll/Rotating bracket-1.STL"  allstll/Rotating_bracket-1.STL
+mv "allstll/Rotating bridge joint-1.STL"  allstll/Rotating_bridge_joint-1.STL
+mv "allstll/screwed board-1.STL"  allstll/screwed_board-1.STL
+mv "allstll/sleeve3.2-3-2.STL"  allstll/sleeve3.2-3-2.STL
+mv "allstll/Sleeve3.2-9-1.STL"  allstll/Sleeve3.2-9-1.STL
+mv "allstll/sleeve8mm-1.STL"  allstll/sleeve8mm-1.STL
+mv "allstll/sleeve for triangle part.STL"  allstll/sleeve_for_triangle_part.STL
+mv "allstll/supporting plate of arm3.0-1.STL"  allstll/supporting_plate_of_arm3.0-1.STL
+mv "allstll/supporting plate of motor(left)-1.STL"  allstll/supporting_plate_of_motor_left-1.STL
+mv "allstll/supporting plate of motor(right).STL"  allstll/supporting_plate_of_motor_right.STL
+mv "allstll/supporting plate of servo -2.STL"  allstll/supporting_plate_of_servo-2.STL
+mv "allstll/the axis paralleled to the major arm-1.STL"  allstll/the_axis_paralleled_to_the_major_arm-1.STL
+mv "allstll/the axis paralleled to the major arm-2.STL"  allstll/the_axis_paralleled_to_the_major_arm-2.STL
+mv "allstll/the axis paralleled to the minor arm-1.STL"  allstll/the_axis_paralleled_to_the_minor_arm-1.STL
+mv "allstll/the baseborad of arm-1.STL"  allstll/the_baseborad_of_arm-1.STL
+mv "allstll/the baseborad of arm.STL"  allstll/the_baseborad_of_arm.STL
+mv "allstll/Triangle linking block-1.STL"  allstll/Triangle_linking_block-1.STL
